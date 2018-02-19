00ab25796101c95db461be4d4c95f79094a5985c
Julius Saputra <julius_saputra@hotmail.com>
2018-02-19 17:41:47 +0100

    some code documentations

--- src/ios/NailPluginSwift.swift
+++ src/ios/NailPluginSwift.swift
@@ -2,21 +2,24 @@ import UIKit
 import MobileCoreServices
 
 @objc(NailPluginSwift) class NailPluginSwift : CDVPlugin {
-    
+
     var command : CDVInvokedUrlCommand?
-    
+
+    ///Main function which would be call to open the extension
     @objc(login:)
     func login(command: CDVInvokedUrlCommand){
+        //set the protocols and and singleton for the extension
         let item = "org.moodle.moodle_mobile_app"
         let singleton = true
         self.command = command
+
         print("IN PLUGIN SWIFT , singleton : \(singleton)")
-        
+
         let activityVC = UIActivityViewController(activityItems: [item, singleton.description], applicationActivities: nil)
         DispatchQueue.main.async {
             self.viewController.present(activityVC, animated: true, completion: nil)
         }
-        
+
         activityVC.completionWithItemsHandler = {
             activityType, completed, returnedItems, error in
             print("BACK FROM EXTENSION")
@@ -29,11 +32,11 @@ import MobileCoreServices
             }
         }
     }
-    
-    
+
+    //Extract the data from the extension
     func extractDataFromExtension(item : NSExtensionItem){
         let itemProvider = item.attachments?.first as! NSItemProvider
-        
+
         if itemProvider.hasItemConformingToTypeIdentifier(kUTTypeJSON as String){
             itemProvider.loadItem(forTypeIdentifier: kUTTypeJSON as String, options: nil, completionHandler: { (data, error) -> Void in
                 if error != nil {
@@ -42,21 +45,22 @@ import MobileCoreServices
                 }
                 let jsonData = data as! Data
                 do{
+                    //returned items is in json format
+                    
                     let dataSource = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String : Any]
                     print("DATA SOURCE : \(dataSource!)")
                     let result = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: dataSource!["api_key"] as! String)
                     self.commandDelegate.send(result, callbackId: self.command?.callbackId)
-                    
+
                 } catch{
                     print(error)
                     return
                 }
             })
-            
+
         }
     }
-    
-    
-    
-}
 
+
+
+}
--- www/nailpluginswift.js
+++ www/nailpluginswift.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 
 var PLUGIN_NAME = 'NailPluginSwift';
-
+//the function which connect java script and native language
 var NailPluginSwift = {
   login: function(phrase, cb) {
     exec(cb, null, PLUGIN_NAME, 'login', [phrase]);
