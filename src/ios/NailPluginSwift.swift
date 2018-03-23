92e8686c11f3c1e6093355f2098c425175fa956e
Julius Saputra <julius_saputra@hotmail.com>
2018-03-23 13:29:54 +0100

    multiple functions added

--- src/ios/NailPluginSwift.swift
+++ src/ios/NailPluginSwift.swift
@@ -4,10 +4,14 @@ import MobileCoreServices
 @objc(NailPluginSwift) class NailPluginSwift : CDVPlugin {
     
     var command : CDVInvokedUrlCommand?
+    private static var nail: IdNativeAppIntegrationLayer?
     
     ///Main function which would be call to open the extension
-    @objc(login:)
-    func login(command: CDVInvokedUrlCommand){
+    @objc(authorizeProtocols:)
+    func authorizeProtocols(command: CDVInvokedUrlCommand){
+        
+        
+        
         //set the protocols and and singleton for the extension
         let singleton = true
         self.command = command
@@ -36,8 +40,11 @@ import MobileCoreServices
         }
     }
     
-    @objc(login2:)
-    func login2(command: CDVInvokedUrlCommand){
+    @objc(authorizeProtocols2:)
+    func authorizeProtocols2(command: CDVInvokedUrlCommand){
+        
+        // var x = self.getNail()
+        //print("NAIL TEXT non Singleton : " , x.getText())
         //set the protocols and and singleton for the extension
         let singleton = false
         self.command = command
@@ -77,29 +84,192 @@ import MobileCoreServices
                     return
                 }
                 let jsonData = data as! Data
-                do{
-                    //returned items is in json format
-                    
-                    let dataSource = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String : Any]
-                    print("DATA SOURCE : \(dataSource!)")
-                    
-                    if let dict = dataSource!["Wall-E"] as? [String : Any] {
-                        let result = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: dict["api_key"] as! String)
-                        self.commandDelegate.send(result, callbackId: self.command?.callbackId)
-                    }else{
-                        let result = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: dataSource!["api_key"] as! String)
-                        self.commandDelegate.send(result, callbackId: self.command?.callbackId)
-                    }
-                    
-                } catch{
-                    print(error)
-                    return
-                }
+                NailPluginSwift.nail = IdNativeAppIntegrationLayer(inputData: jsonData)
+                
+                //returned items is in json format
+                let response = self.getNail()!.serialize()
+                print("DATA SOURCE : \(response)")
+                
+                let x = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: response)
+                self.commandDelegate.send(x, callbackId: self.command?.callbackId)
             })
-            
         }
     }
     
+    @objc(serviceNames:)
+    func serviceNames(commandItem: CDVInvokedUrlCommand){
+        
+        guard let nailTmp = getNail() else {
+            return
+        }
+        
+        let names = nailTmp.getServiceNames()
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: names)
+        self.commandDelegate.send(pluginResult, callbackId: commandItem.callbackId)
+        
+    }
+    
+    @objc(getEndpointUrl:)
+    func getEndpointUrl(commandItem: CDVInvokedUrlCommand){
+        
+        guard let nailTmp = getNail() else{
+            return
+        }
+        
+        let arg = commandItem.arguments.first as! [String: Any]
+        
+        guard let serviceName = arg["serviceName"] as? String else {
+            print("getEndpointUrl: 'serviceName' Key is not found in Dictionary")
+            return
+        }
+        guard let prtcl = arg["protocolName"] as? String else{
+            print("getEndpointUrl: 'protocolName' Key is not found in Dictionary")
+            return
+            
+        }
+        
+        let endpoint = nailTmp.getEndpointUrl(serviceName: serviceName, protocolName: prtcl)
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: endpoint)
+        self.commandDelegate.send(pluginResult, callbackId: commandItem.callbackId)
+        
+    }
+    
+    @objc(getDisplayName:)
+    func getDisplayName(commandItem: CDVInvokedUrlCommand){
+        guard let nailTmp = getNail() else {
+            return
+        }
+        
+        let arg = commandItem.arguments.first as! [String: Any]
+        guard let serviceName = arg["serviceName"] as? String else {
+            print("getDisplayName: 'serviceName' Key is not found in Dictionary")
+            return
+        }
+        
+        let displayname = nailTmp.getDisplayName(serviceName: serviceName)
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: displayname)
+        self.commandDelegate.send(pluginResult, callbackId: commandItem.callbackId)
+    }
+    
+    @objc(getServiceToken:)
+    func getServiceToken(commandItem: CDVInvokedUrlCommand){
+        
+        guard let nailTmp = getNail() else {
+            return
+        }
+        
+        let arg = commandItem.arguments.first as! [String: Any]
+        
+        guard let serviceName = arg["serviceName"] as? String else{
+            print("getServiceToken: 'serviceName' Key is not found in Dictionary")
+            return
+        }
+        guard let prtcl = arg["protocolName"] as? String else{
+            print("getServiceToken: 'protocolName' Key is not found in Dictionary")
+            return
+        }
+        
+        let token = nailTmp.getAccessToken(serviceName: serviceName, protocolName: prtcl)
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: token)
+        self.commandDelegate.send(pluginResult, callbackId: commandItem.callbackId)
+    }
+    
+    @objc(getServiceUrl:)
+    func getServiceUrl(commandItem: CDVInvokedUrlCommand){
+        
+        guard let nailTmp = getNail() else {
+            let pluginRes  = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Login is required to use this function")
+            self.commandDelegate.send(pluginRes, callbackId: commandItem.callbackId)
+            return
+        }
+        
+        let arg = commandItem.arguments.first as! [String : Any]
+        guard let serviceName = arg["serviceName"] as? String else{
+            print("getServiceUrl: 'serviceName' Key is not found in Dictionary")
+            return
+        }
+        
+        let serviceUrl = nailTmp.getServiceUrl(serviceName: serviceName)
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: serviceUrl)
+        self.commandDelegate.send(pluginResult, callbackId: commandItem.callbackId)
+        
+    }
+    
+    @objc(removeService:)
+    func removeService(commandItem: CDVInvokedUrlCommand){
+        
+        guard let nailTmp = getNail() else {
+            let pluginRes  = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Login is required to use this function")
+            self.commandDelegate.send(pluginRes, callbackId: commandItem.callbackId)
+            return
+        }
+        
+        let arg = commandItem.arguments.first as! [String : Any]
+        guard let serviceName = arg["serviceName"] as? String else{
+            print("getServiceUrl: 'serviceName' Key is not found in Dictionary")
+            return
+        }
+        
+        nailTmp.removeService(serviceName: serviceName)
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
+        self.commandDelegate.send(pluginResult, callbackId: commandItem.callbackId)
+        
+    }
+    
+    @objc(clearAllServices:)
+    func clearAllServices(commandItem: CDVInvokedUrlCommand){
+        guard let nailTmp = getNail() else {
+            return
+        }
+        nailTmp.clearAllService()
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
+        self.commandDelegate.send(pluginResult, callbackId: commandItem.callbackId)
+    }
+    
+    @objc(serialize:)
+    func serialize(commandItem: CDVInvokedUrlCommand){
+        guard let nailTmp = getNail() else {
+            return
+        }
+        let serializedString = nailTmp.serialize()
+        if serializedString.count == 0 {
+            return
+        }
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: serializedString)
+        self.commandDelegate.send(pluginResult, callbackId: commandItem.callbackId)
+    }
+    
+    @objc(parse:)
+    func parse(commandItem: CDVInvokedUrlCommand){
+        let arg = commandItem.arguments.first as! [String : Any]
+        guard let serviceSpec = arg["serviceSpec"] as? String else{
+            print("parse: 'serviceSpec' Key is not found in Dictionary")
+            return
+        }
+        //check if the string is empty or not
+        if serviceSpec.count == 0 {
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
+            self.commandDelegate.send(pluginResult, callbackId: commandItem.callbackId)
+        }
+        
+        let nailTmp = IdNativeAppIntegrationLayer(serializedStr: serviceSpec)
+        if nailTmp.getServiceNames().count == 0 {
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
+            self.commandDelegate.send(pluginResult, callbackId: commandItem.callbackId)
+        }else {
+            NailPluginSwift.nail = nailTmp
+            let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
+            self.commandDelegate.send(pluginResult, callbackId: commandItem.callbackId)
+        }
+    }
+    
+    func getNail()-> IdNativeAppIntegrationLayer? {
+        if NailPluginSwift.nail == nil{
+            return nil
+        }
+        return NailPluginSwift.nail!
+        
+    }
     
     
 }
